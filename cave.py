a2602150833195980ce40e404f3c9b30e5a835af
kennerdogerlino <ken.quisquino@education.nsw.gov.au>
2025-05-30 00:41:52 +0000

    Added more methods to cave class

--- cave.py
+++ cave.py
@@ -2,3 +2,15 @@ class Cave:
     def __init__(self, cave_name):
         self.name = cave_name
         self.description = None
+
+    def set_name(self, cave_name):
+        self.name = cave_name    
+
+    def set_description(self, cave_description):
+        self.description = cave_description
+
+    def get_description(self):
+        return self.description
+    
+    def describe(self):
+        print(self.description)
