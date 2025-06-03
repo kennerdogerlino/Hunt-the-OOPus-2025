0ca6a046177864c91806651473357207bc9dd78b
kennerdogerlino <ken.quisquino@education.nsw.gov.au>
2025-06-03 02:11:40 +0000

    Added code move between different objects

--- cave.py
+++ cave.py
@@ -2,16 +2,36 @@ class Cave:
     def __init__(self, cave_name):
         self.name = cave_name
         self.description = None
-        self.linked_caves = 
+        self.linked_caves = {}
 
     def set_name(self, cave_name):
-        self.name = cave_name    
+        self.name = cave_name
+
+    def get_name(self):
+        return self.name
 
     def set_description(self, cave_description):
         self.description = cave_description
 
     def get_description(self):
         return self.description
-    
+
     def describe(self):
         print(self.description)
+
+    def link_caves(self, cave_to_link, direction):
+        self.linked_caves[direction] = cave_to_link
+        #print(self.name + "linked caves:" + repr(self.linked_caves))
+
+    def get_details(self):
+        print(self.name)
+        print("====================")
+        for direction, cave in self.linked_caves.items():
+            print("The " + cave.get_name() + " is " + direction)
+
+    def move(self, direction):
+        if direction in self.linked_caves:
+            return self.linked_caves[direction]
+        else:
+            print("You dingus, can't go that way bro")
+            return self
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from cave import Cave
 
 cavern = Cave("Cavern")
-cavern.set_description("A damp and dirty cave")
+cavern.set_description("A damp and dirty cave.")
 grotto = Cave("Grotto")
+grotto.set_description("A small cave with ancient markings.")
 dungeon = Cave("Dungeon")
-cavern.describe()
+dungeon.set_description("A Large cave with a rack.")
+
+cavern.link_caves(dungeon, "South")
+dungeon.link_caves(cavern, "North")
+dungeon.link_caves(grotto, "West")
+grotto.link_caves(dungeon, "East")
+
+current_cave = cavern
+while True:
+    print("\n")
+    current_cave.get_details()
+    command = input("> ")
+    current_cave = current_cave.move(command)
+    
