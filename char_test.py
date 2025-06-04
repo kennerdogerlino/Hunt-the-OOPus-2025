b015a207cf9e06807b5a964d77a05e2603fff81e
kennerdogerlino <ken.quisquino@education.nsw.gov.au>
2025-06-04 01:18:55 +0000

    added enemey weakness and fight method

--- char_test.py
+++ char_test.py
@@ -1,8 +1,12 @@
 """imports character to Character"""
 from character import Enemy
 
-joyboy = Enemy("joyboy", "A dirty, smelly Bot")
-joyboy.describe()
-joyboy.set_conversation("Come closer little one. I cannot see you.")
-joyboy.talk()
-print(joyboy)
+Joyboy = Enemy("Joyboy", "A dirty, smelly Bot")
+Joyboy.describe()
+Joyboy.set_conversation("Come closer little one. I cannot see you.")
+Joyboy.talk()
+print(Joyboy)
+Joyboy.set_weakness("touching grass")
+
+fight_with = input("What you fight with cuh? ")
+Joyboy.fight(fight_with)
--- character.py
+++ character.py
@@ -27,7 +27,24 @@ class Character:
         """Allows characters to fight with the player"""
         print(self.name + " doe not want to fight with you.")
         return True
+
 class Enemy(Character):
     """Creates enemey class"""
     def __init__(self, char_name, char_description):
         super().__init__(char_name, char_description)
+        self.weakness = None #he too storng bro, he joybjoy fr
+
+    def set_weakness(self, item_weakness):
+        """Sets enemy weakness"""
+        self.weakness = item_weakness
+
+    def get_weakness(self):
+        """Gets enemy weakness"""
+        return self.weakness
+
+    def fight(self, combat_item):
+        if combat_item == self.weakness:
+            print("\nYou fend off " + self.name + " with the " + combat_item)
+            return True
+        else:
+            print(self.name + " swallows you whole! You died.")
