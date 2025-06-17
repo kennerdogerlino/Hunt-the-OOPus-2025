cd54070258a30de27b0b22ce43a581294ff3d1a9
kennerdogerlino <ken.quisquino@education.nsw.gov.au>
2025-06-17 03:50:15 +0000

    fixed changing caves to areas

--- main.py
+++ main.py
@@ -1,36 +1,60 @@
-"""Imports cave to Cave"""
+"""Imports cave to Area"""
 import os
-from cave import Cave
+from area import Area
 from character import Enemy
 
-cavern = Cave("Cavern")
+cavern = Area("Cavern")
 cavern.set_description("A damp and dirty cave.")
-grotto = Cave("Grotto")
+grotto = Area("Grotto")
 grotto.set_description("A small cave with ancient markings.")
-dungeon = Cave("Dungeon")
+dungeon = Area("Dungeon")
 dungeon.set_description("A Large cave with a rack.")
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 
 chocolate10992 = Enemy("chocolate10992", "A dirty, smelly Bot")
 chocolate10992.set_conversation("Come closer little one. I cannot see you.")
 chocolate10992.set_weakness("touching grass")
 dungeon.set_character(chocolate10992)
 
-cavern.link_caves(dungeon, "South")
-dungeon.link_caves(cavern, "North")
-dungeon.link_caves(grotto, "West")
-grotto.link_caves(dungeon, "East")
-
-current_cave = cavern
+current_area = cavern
 dead = False
 while dead == False:
     print("\n")
-    current_cave.get_details()
-    inhabitated = current_cave.get_character()
+    current_area.get_details()
+    inhabitated = current_area.get_character()
     if inhabitated is not None:
         inhabitated.describe()
     command = input("> ")
     if command in ["North", "East", "South", "West"]:
-        current_cave = current_cave.move(command)
+        current_area = current_area.move(command)
     elif command == "Talk":
         if inhabitated is not None:
             inhabitated.talk()
