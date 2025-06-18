eeecada9a70ca38446072d6ddf3ff83d4b60a76b
kennerdogerlino <ken.quisquino@education.nsw.gov.au>
2025-06-18 01:30:29 +0000

    worked on excel map and started to put areas in my code

--- main.py
+++ main.py
@@ -3,50 +3,44 @@ import os
 from area import Area
 from character import Enemy
 
-cavern = Area("Cavern")
-cavern.set_description("A damp and dirty cave.")
-grotto = Area("Grotto")
-grotto.set_description("A small cave with ancient markings.")
-dungeon = Area("Dungeon")
-dungeon.set_description("A Large cave with a rack.")
+def clear_console():
+    """Clears the console in terminal"""
+    os.system("cls" if os.name == "nt" else "clear")
 
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+#Spawnpoint (can respawn when dead)
+Wooden_house = Area("Wooden House")
+
+#Level 1 forest biome section
+Forest_entrance = Area("forest entrance")
+Giant_Tree = Area("Giant Tree")
+Platforms = Area("Platforms")
+Abandoned_Room  = Area("Abandoned room")
+
+#Level 2 Jungle Biome section
+Jungle_Biome_entrance = Area("Wooden House")
+Jungle_ravine = Area("Wooden House")
+#South/down direction from jungle ravine
+Vine_covered_wall = Area("Wooden House")
+GOLDEN_CHEST = Area("Wooden House")
+Golden_chest = Area("Wooden House")
+#West direction going past ravine
+Rainforest = Area("Wooden House")
+
+#Level 3 Hallowed Biome section
+Hallowed_Biome_entrance
+
+#Link_areas
+Wooden_house.link_areas()
 
 chocolate10992 = Enemy("chocolate10992", "A dirty, smelly Bot")
 chocolate10992.set_conversation("Come closer little one. I cannot see you.")
 chocolate10992.set_weakness("touching grass")
-dungeon.set_character(chocolate10992)
+Wooden_house.set_character(chocolate10992)
 
-current_area = cavern
-dead = False
-while dead == False:
+
+current_area = Wooden_house
+DEAD = False
+while DEAD is not False:
     print("\n")
     current_area.get_details()
     inhabitated = current_area.get_character()
@@ -64,11 +58,7 @@ while dead == False:
             if inhabitated.fight(fight_with) is True:
                 print("W's in the chat! You win the battle")
             else:
-                print("!Blasphemy! You lost the fight.")
-                dead = True
+                print("Flabbergastinations! You lost the fight.")
+                DEAD = True
         else:
             print("There is no one here to fight with")
-
-    def clear_console():
-        """Clears the console in terminal"""
-        os.system("cls" if os.name == "nt" else "clear")
